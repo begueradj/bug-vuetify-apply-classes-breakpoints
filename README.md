08750e47440d4f401a5064beae6ddad4de738117
begueradj <begueradj.bill@gmail.com>
2019-03-15 10:29:47 +0200

    Applying Vuetify classes depending on breakpoints

---
+++ README.md
@@ -0,0 +1,18 @@
+## vuetify-apply-classes-breakpoints
+
+Vuetify: applying classes depending on the breakpoints behaves randomly.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+```
+
+
+## Author
+Billal Begueradj
---
+++ components/SubComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container
+    grid-list-md
+    text-xs-center
+    fill-height
+  >
+    <v-layout
+      row
+      wrap
+      align-center
+    >
+      <v-flex
+        xs12
+        sm4
+        :class="{
+          'display-3': $vuetify.breakpoint.smAndUp,
+          'red--text': $vuetify.breakpoint.xs
+        }"
+      >
+        Sub component
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'SubComponent'
+}
+</script>
